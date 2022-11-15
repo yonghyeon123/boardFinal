4cb3ed3ea043b532ca38ff5eb2c8ca715c7483c1
yonghyeon123 <mnbvcx0229@gmail.com>
2022-11-15 17:53:57 +0900

    2022-11-15 AWS 서버 빌드

--- src/main/java/com/aia/boardFinal/BoardFinalApplication.java
+++ src/main/java/com/aia/boardFinal/BoardFinalApplication.java
@@ -2,11 +2,17 @@ package com.aia.boardFinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource("classpath:db.properties")
 @SpringBootApplication
-public class BoardFinalApplication {
+public class BoardFinalApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(BoardFinalApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardFinalApplication.class, args);
--- src/main/java/com/aia/boardFinal/controller/BoardController.java
+++ src/main/java/com/aia/boardFinal/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.aia.boardFinal.controller;
 
+import com.aia.boardFinal.model.UserDTO;
 import com.aia.boardFinal.service.BoardService;
 import com.fasterxml.jackson.databind.deser.std.ObjectArrayDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
